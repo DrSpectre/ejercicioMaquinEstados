57f35849c28ddf024fb0640c1a90e10cd968cbf0
Fernando H. G.B. <bluakorvo@protonmail.com>
2024-02-13 16:34:01 -0700

    [INIT] Inicializando el documento

---
+++ js/maquina_estados.js
@@ -0,0 +1,15 @@
+
+const MaquinaEstados = {
+    estado_actual: 'texto por aqui',
+
+    actualizar: (evento) => {
+        console.log(evento);
+    },
+
+    cambiar_estado: (estado_nuevo) => {
+        console.log(evento);
+
+    }
+
+}
+
