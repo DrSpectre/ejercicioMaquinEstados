d00b4a7140e178fe0855a78370f8aeb33e3110cf
Fernando H. G.B. <bluakorvo@protonmail.com>
2024-02-15 16:48:25 -0700

    [Agrega] un estado nuevo y mejora el estado base

--- js/estados.js
+++ js/estados.js
@@ -3,15 +3,38 @@ const estado_base = {
     inicializar: () => {
         document.querySelector("#mostrar_estado").innerText = "Esta en estado base"
 
+        document.querySelector("#boton1").innerText = "Cambiar a Llorar por comida"
+        document.querySelector("#boton2").innerText = "Cambiar a Dormir"
     },
-    actualizar: () => {
+    actualizar: (evento) => {
         document.querySelector("#mostrar_estado").innerText = "Pulsaste el boton"
+        console.log(evento.target)
 
+        if(evento.target && evento.target.id === "boton1"){
+            console.log("HOuston estamos bien")
+            MaquinaEstados.cambiar_estado(llorar_por_comida)
+        }
+        else {
+            console.log("Houston tenemos un problema")
+
+        }
     },
     finalizar: () => {
-
+        document.querySelector("#mostrar_estado").innerText = "Esta saliendo del estado base"
     },
+}
 
+
+const llorar_por_comida = {
+    inicializar: () => {
+        document.querySelector("#mostrar_estado").innerText = "Esta llorando por comida"
+    },
+    actualizar: () => {
+        //document.querySelector("#mostrar_estado").innerText = ""
+    },
+    finalizar: () => {
+        document.querySelector("#mostrar_estado").innerText = "Esta saliendo del estado llorar por comida"
+    },
 }
 
 
