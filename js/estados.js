4fb1dc1fd3924732fcbfbf7e6c0869438d72c78c
Fernando H. G.B. <bluakorvo@protonmail.com>
2024-02-15 15:58:34 -0700

    [Agrega] el archivo donde viviran los estados y el estado base

---
+++ js/estados.js
@@ -0,0 +1,26 @@
+
+const estado_base = {
+    inicializar: () => {
+        document.querySelector("#mostrar_estado").innerText = "Esta en estado base"
+
+    },
+    actualizar: () => {
+        document.querySelector("#mostrar_estado").innerText = "Pulsaste el boton"
+
+    },
+    finalizar: () => {
+
+    },
+
+}
+
+
+
+
+
+
+
+
+
+
+
